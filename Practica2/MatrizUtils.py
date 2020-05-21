c54aaf0c9a4d4cda5f0f50aca5494a337a48ba3b
Jormii <lopeznatalj@gmail.com>
2020-05-21 12:53:47 +0200

    Limpiado MatrizUtils.py

--- Practica2/MatrizUtils.py
+++ Practica2/MatrizUtils.py
@@ -1,7 +1,8 @@
 import importlib
 
-i_vegas = importlib.import_module("LasVegas")
 i_casilla = importlib.import_module("Casilla")
+i_vegas = importlib.import_module("LasVegas")
+
 
 def pertenece_a_matriz(posicion, ancho, alto):
     x = posicion[0]
@@ -15,6 +16,7 @@ def se_saldria_de_la_matriz(posicion, direccion, ancho, alto):
     return not pertenece_a_matriz((x, y), ancho, alto)
 
 
+# Utilizada en los giros de los tuneles
 def calcular_nueva_direccion(posicion, direccion, ancho, alto):
     x = posicion[0]
     y = posicion[1]
@@ -22,13 +24,13 @@ def calcular_nueva_direccion(posicion, direccion, ancho, alto):
 
     # Se mueve hacia el norte o sur
     if direccion[0] == 0:
-        # %x € [0, 1], si extremo izquierdo/derecho
+        # %x € {0, 1}, si extremo izquierdo/derecho
         porcentaje_en_x = x / (ancho - 1) * 100
         girar_derecha = i_vegas.random_las_vegas(0, 100) > porcentaje_en_x
         nueva_direccion = direcciones[1] if girar_derecha else direcciones[3]
     # Se mueve hacia el oeste o este
     else:
-        # %y € [0, 1], si extremo superior/inferior
+        # %y € {0, 1}, si extremo superior/inferior
         porcentaje_en_y = y / (alto - 1) * 100
         girar_abajo = i_vegas.random_las_vegas(0, 100) > porcentaje_en_y
         nueva_direccion = direcciones[2] if girar_abajo else direcciones[0]
